f375bad666345086714f6a347293752a8af2e2e4
Juliya Miller <juliyagrin35@gmail.com>
2021-12-10 15:31:43 +0200

    get

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const body_parser = require('body-parser');
+
 const app = express();
+const users = require('./db/user.json');
 
 const PORT = 8030;
 
 app.get('/', (req, res) => {
-    res.send({'massage':'ALL work!'})
+    res.send(users);
+  });
+
+  app.get('/:id', (req, res) => {
+    userId = req.params.id;
+
+    if(req.method === "GET")
+    {
+      for(i in users.users){
+        (users.users[i].id == userId) && res.status(200).send(users.users[i]);
+      }   
+
+    }
+    res.status(405).send({'message': 'asd'});
   })
   
   app.listen(PORT, () => {
