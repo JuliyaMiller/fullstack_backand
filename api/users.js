b1d048cdd63164b994f836d234c46b103b5ffdb7
Julia Miller <89014505+JuliyaMiller@users.noreply.github.com>
2022-01-17 15:33:56 +0200

    put

--- api/users.js
+++ api/users.js
@@ -1,11 +1,46 @@
 const mongoose = require('mongoose');
 const Router = require('express').Router;
-
-
 const UserRouter = Router();
 
-UserRouter.get('/', (req, res) => {
-    res.send('hello');
+const CONNECTION_STRING = 'mongodb+srv://todoAdmin:1111@cluster0.oxik2.mongodb.net/userslist?retryWrites=true&w=majority'
+const UserSchema = new mongoose.Schema({
+// id: Number,
+  password: String,
+  login: String,
+  age: Number  
+});
+const UserModel = mongoose.model('user', UserSchema);
+mongoose.connect(CONNECTION_STRING);
+
+UserRouter.get('/', async (req, res) => {
+    const data = await UserModel.find({});
+    res.send(data);
+});
+
+UserRouter.post('/', async (req, res) => {
+    if(req.body.login && req.body.password){
+        const user = new UserModel(req.body);
+        const result = await user.save();
+        res.send(result);
+    }else{
+        return res.status(403).send("Error");
+    }
 })
 
-module.exports = UserRouter();
+UserRouter.get('/:id', async (req, res) => {
+    const result = await UserModel.findById(req.params.id);
+    res.send(result);
+});
+
+UserRouter.delete('/:id', async (req, res) => {
+    const result = await UserModel.findByIdAndDelete(req.params.id);
+    res.send(result);
+});
+
+UserRouter.put('/:id', async (req, res) => {
+    const data = req.body;
+    const result = await UserModel.findByIdAndUpdate(req.params.id, data);
+    res.send(result);
+})
+
+module.exports = UserRouter;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8030;
+
 const mockapp = require('./api/mockapp');
 const userRouter = require('./api/users'); 
 
