0557d2c6bac5531f96f4b3e067cb936b3427f616
Julia Miller <89014505+JuliyaMiller@users.noreply.github.com>
2022-01-17 11:53:58 +0200

    mockapp

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8030;
-const userRouter = require('./api/users');
+const mockapp = require('./api/mockapp');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', userRouter);
+app.use('/mockapp', mockapp);
 
   app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
